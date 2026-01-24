6defeb505a98a9f850040366fa22f104fdce2340
puntahermosaconnect <puntahermosaconnect@gmail.com>
2026-01-23 20:07:24 -0500

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ph.connect',
+  appName: 'Punta Hermosa Connect',
+  webDir: 'dist/public'
+};
+
+export default config;
